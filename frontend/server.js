830de820436c2aadb59ca3b50865f8fd2a405243
mikolajkozlowskiii <mikolajkozlowskiii@gmail.com>
2024-04-22 09:23:14 +0200

    Bridge network

--- frontend/server.js
+++ frontend/server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static('public'));
 
-const targetAddress = process.env.PROXY_TARGET || 'http://host.docker.internal:8080';
+const targetAddress = process.env.PROXY_TARGET || 'http://localhost:8080';
 
 
 app.use('/game', createProxyMiddleware({ 
